ca31bddf93af738c1cba93c68e0f096cd3339ba2
Shao-Ting Chiu <stchiu@email.tamu.edu>
2024-05-09 19:59:17 -0500

    wip: S1S2 loss

--- mmpdenet/loss.py
+++ mmpdenet/loss.py
@@ -1,6 +1,8 @@
 """
 Loss function for solving MMPDE Problem
 """
+import jax 
+import jax.numpy as jnp
 from jax.typing import ArrayLike
 from typing import Any, Callable, NamedTuple
 
@@ -20,3 +22,31 @@ def loss_MMPDE(Xc:Points, tranFn:TransFn, monitorFn:MonitorFn)->float:
     """
     pass
 
+def diff(Xc, tranFn, monitorFn):
+    mJ_func = jax.jacfwd(tranFn)
+
+    j = jnp.linalg.det(mJ_func(Xc))
+    w = monitorFn(Xc)
+    
+    def s12(Xc):
+        mJ = mJ_func(Xc)
+        xy_xi = mJ[0,:]
+        xy_eta = mJ[1,:]
+        output = xy_xi * xy_eta.T
+        output = output / (j*w)
+        return xy_xi, xy_eta
+    
+
+
+def S1(Xc, tranFn, monitorFn)->float:
+    """Calculate S1 (Eq. 15)
+
+    Args:
+        xi (float): xi value
+        eta (float): eta value
+        tranFn (TransFn): Transformation function from computational domain to physical domain
+
+    Returns:
+        float: S1 value
+    """
+    pass
